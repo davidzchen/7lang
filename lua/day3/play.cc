aad44f1c725106cf0a2d083589b70e8d0a781122
David Z. Chen <david@davidzchen.com>
2015-09-01 06:59:26 -0700

    Lua Day 3: Remove one_note_song.lua. Wrap RtMidiOut with singleton. Print error message if there is an error in Lua script.

--- lua/day3/play.cc
+++ lua/day3/play.cc
@@ -1,14 +1,51 @@
 #include <iostream>
+#include <cstdint>
 #include <vector>
 
 extern "C" {
-#include "lua.h"
-#include "lauxlib.h"
-#include "lualib.h"
+#include "external/lua/lua-5.3.1/src/lua.h"
+#include "external/lua/lua-5.3.1/src/lauxlib.h"
+#include "external/lua/lua-5.3.1/src/lualib.h"
 }
-#include "RtMidi.h"
+#include "external/rtmidi/RtMidi.h"
 
-static RtMidiOut midi;
+// Singleton class that wraps RtMidiOut.
+//
+// For simplicity, the book uses a global RtMidiOut variable. We use a singleton
+// wrapper class instead.
+class MidiOut {
+ public:
+  static MidiOut* Get() {
+    if (instance_ == nullptr) {
+      instance_ = new MidiOut();
+    }
+    return instance_;
+  }
+
+  bool Open(uint32_t port) {
+    uint32_t ports = midi_.getPortCount();
+    if (ports < 1) {
+      std::cerr << "No ports available." << std::endl;
+      return false;
+    }
+    midi_.openPort(port);
+    return true;
+  }
+
+  void Send(uint8_t number, uint8_t note, uint8_t velocity) {
+    std::vector<uint8_t> message{number, note, velocity};
+    midi_.sendMessage(&message);
+  }
+
+ private:
+  MidiOut() {}
+
+  static MidiOut* instance_;
+
+  RtMidiOut midi_;
+};
+
+MidiOut* MidiOut::instance_ = nullptr;
 
 // Sends a MIDI note.
 int midi_send(lua_State* lua) {
@@ -16,12 +53,9 @@ int midi_send(lua_State* lua) {
   double data1 = lua_tonumber(lua, -2);
   double data2 = lua_tonumber(lua, -1);
 
-  std::vector<unsigned char> message(3);
-  message[0] = static_cast<unsigned char>(status);
-  message[1] = static_cast<unsigned char>(data1);
-  message[2] = static_cast<unsigned char>(data2);
-  midi.sendMessage(&message);
-
+  MidiOut::Get()->Send(static_cast<uint8_t>(status),
+                       static_cast<uint8_t>(data1),
+                       static_cast<uint8_t>(data2));
   return 0;
 }
 
@@ -31,13 +65,10 @@ int main(int argc, const char** argv) {
     return -1;
   }
 
-  // Find a running synthesizer using the rtmidi API.
-  unsigned int ports = midi.getPortCount();
-  if (ports < 1) {
-    std::cerr << "No ports available." << std::endl;
+  // Find a running synthesizer.
+  if (!MidiOut::Get()->Open(0)) {
     return -1;
   }
-  midi.openPort(0);
 
   // Initialize the Lua API.
   lua_State* lua = luaL_newstate();
@@ -49,7 +80,12 @@ int main(int argc, const char** argv) {
   lua_setglobal(lua, "midi_send");
 
   // Run the provided Lua file.
-  luaL_dofile(lua, argv[1]);
+  if (luaL_dofile(lua, argv[1]) != 0) {
+    // Exercise(medium, 2): Report any error information returned from the Lua
+    // interpreter.
+    std::cerr << "Error playing " << argv[1] << ": "
+              << lua_tostring(lua, -1) << std::endl;
+  }
 
   // Close the Lua API.
   lua_close(lua);
